0ee61f0c727511ea7d4e4e128a226b2331d70ed3
noefortuna <fortunanoelia6@gmail.com>
2025-08-13 11:28:27 +0000

    noeefort

---
+++ noeefort.py
@@ -0,0 +1,15 @@
+class padre:
+    apellido=´¨
+    def __init__(self,nombre):
+        self,nombre=nombre
+        self
+
+        class hijo(padre):
+            def_init_(self,nombre,lenguajesdeprogramacion):
+                suoer()._init_(nombre)
+
+                padre= padre("nombre del padre)
+                padre.apellido="apellido del padre"
+                hijo = hijo("nombre del hijo","pyton"))
+                print(hijo.nombre,hijo.apellido)
+                
